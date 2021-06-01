3e0df81aefe19b4e65c029826f7c286d0d0989e8
Carlos Dominguez Padron <carlos@theagilemonkeys.com>
2021-06-01 15:13:15 +0100

    fixed cdk and booster versions (#11)

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ export const createLambda = (
   environment?: Record<string, string>
 ): lambda.Function => {
   return new lambda.Function(stack, name, {
-    runtime: lambda.Runtime.NODEJS_12_X,
+    runtime: lambda.Runtime.NODEJS_14_X,
     timeout: Duration.minutes(15),
     memorySize: 1024,
     handler: handler,
